f794b350e8a3c1d6c6d9708659718a51bf70ac56
Alex Brandt <alunduil@alunduil.com>
2010-09-11 13:06:50 -0500

    Empty project creation.

---
+++ build/lib/yaml/CMakeFiles/yamlparser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/yamlparser.dir/yamlspriteparser.cpp.o"
+  "libyamlparser.pdb"
+  "libyamlparser.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/yamlparser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/src/CMakeFiles/partum-artificium.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/partum-artificium.dir/main.cpp.o"
+  "CMakeFiles/partum-artificium.dir/partumartificium.cpp.o"
+  "partum-artificium.pdb"
+  "partum-artificium"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/partum-artificium.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
